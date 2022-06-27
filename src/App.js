e9a290fdeb7b49d73a7c8b340d225347f9712cdb
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-27 19:06:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Be a savage with react
         </a>
       </header>
     </div>
