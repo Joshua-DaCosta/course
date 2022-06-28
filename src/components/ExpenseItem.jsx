7b8bf1014d9e8bbd8241aff858fee6df71348e0e
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-27 20:38:45 -0400

    used props in expense item component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import './ExpenseItem.css';
 
-const ExpenseItem = ({date, title, amount}) => {
-
-
-
+const ExpenseItem = ({ key, title, amount, date }) => {
   return (
-    <div className="expense-item">
+    <div id={key} className="expense-item">
       <div>
         <div>{date}</div>
       </div>
-      <h2 className="expense-item__description">Title</h2>
-      <div className="expense-item__price">Amount</div>
+      <h2 className="expense-item__description">{title}</h2>
+      <div className="expense-item__price">{amount}</div>
     </div>
   );
 };
