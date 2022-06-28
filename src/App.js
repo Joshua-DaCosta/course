28c041fb49a0424e3d5ca14f809e6e88b2fc3812
Joshua Da Costa <Joshua.0331@hotmail.com>
2022-06-27 20:37:04 -0400

    passes expense data as props via data file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import expenses from './data/expenses';
 
 function App() {
+
   return (
-    <div className='app'>
-      <h1>Let's get started!</h1>
-      <ExpenseItem />
+    <div className="app">
+      <h1>Expense Tracker</h1>
+      {expenses.map(({id, title, amount, date}) => {
+      return <ExpenseItem key={id} date={date} title={title} amount={amount} />;
+      })}
+      
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({date, title, amount}) => {
+
+
+
   return (
     <div className="expense-item">
       <div>
-        <div>Date</div>
+        <div>{date}</div>
       </div>
       <h2 className="expense-item__description">Title</h2>
       <div className="expense-item__price">Amount</div>
